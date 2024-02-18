c6b9f3b8a19eb28600b1ef3b494d6757a5ead088
Jessica Gates <gatespj@outlook.com>
2024-02-18 12:36:06 -0500

    initial commit with vs project

---
+++ MovieDatabase/MovieDatabase/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase
+{
+    internal class Movie
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+    }
+    // need a constructor here - accept title and category as parameters and use to pass in to initialize the variables
+}
